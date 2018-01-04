1e0f572af083165692c76541d789f713ced4769e
Legendary Ma <18482571+pchao6@users.noreply.github.com>
2018-01-04 17:30:48 +0800

    unittest study

---
+++ unittest/run.py
@@ -0,0 +1,6 @@
+import unittest
+from unittest1 import CalTest1
+from unittest2 import CalTest2
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ unittest/unittest2.py
@@ -0,0 +1,18 @@
+import unittest
+
+def cal(a, b):
+    return a*b
+
+class CalTest2(unittest.TestCase):
+    def testA(self):
+        expected = 6
+        result = cal(2, 4)
+        self.assertEqual(expected, result)
+
+    def testB(self):
+        expected = 0
+        result = cal(2, 0)
+        self.assertEqual(expected, result)
+
+if __name__ == "__main__":
+    unittest.main()
